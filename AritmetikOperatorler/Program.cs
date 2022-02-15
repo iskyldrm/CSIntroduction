1424639ea07fb215327fd85b317b7ce2fabe72be
iskyldrm <isakyildirim@outlook.com>
2022-02-15 19:20:43 +0300

    Operatörler anlatıldı

---
+++ AritmetikOperatorler/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AritmetikOperatorler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //atama operatörü
+            //degiskenTipi degiskenAdi = <--(atama operatörü)  veri
+
+
+            #region Aritmetik Operatorler
+
+            
+            /*
+            + toplama
+            - çıkarma
+            * çarpma
+            / bölme
+            % mod operatörü
+
+            sayi++ : artirma operatörü=> birer birer artırı
+            sayi-- : azaltma operatörü=> birer birer azaltır
+            ++sayi : başka işlem varsa önce sayiyi arttırı sonra diğer işleme geçer
+            --sayi : başka işlem varsa önce sayiyi azaltır sonra diğer işleme geçer
+
+             
+             */
+            #endregion
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
