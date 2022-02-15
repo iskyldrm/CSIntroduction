7c420557c8dfe170809e043bc4a1251be4bbb791
iskyldrm <isakyildirim@outlook.com>
2022-02-15 15:44:36 +0300

    Add project files.

---
+++ FirstConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using System;
+
+namespace FirstConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("DENEME");
+            Console.ReadLine();
+        }
+    }
+}
