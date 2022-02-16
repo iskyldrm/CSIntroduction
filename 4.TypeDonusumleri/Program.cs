9610a1dfe59f14949b2ff58f3762e5316b40fc99
iskyldrm <isakyildirim@outlook.com>
2022-02-16 16:43:44 +0300

    Yeni proje oluşturuldu tipdonusumleri anlatıldı datetime veri tipi anlatıldı

---
+++ 4.TypeDonusumleri/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace _4.TypeDonusumleri
+{
+    class Program
+    {
+        /*
+         .NET'te iki tip tür dönüşümü vardır
+
+        1- kapalı donüşüm (implicit conversion)
+        2- acık donüşüm   (Explicit conversion)
+
+         
+         */
+
+        static void Main(string[] args)
+        {
+
+            //convert sınıfı tip donusumu için kullanılan .NET içerisindeki sınıftır
+            //to ile başlayan metodlar kullanılabilir
+
+            //bool,byte,char,datetime,decimal ve parse metodlarıda kullanılır.
+
+
+            string deger = "15";
+
+            int sayi = int.Parse(deger);
+
+            int isayi = 9579;
+
+            byte bsayi = 125;
+
+            bsayi = (byte)isayi;
+
+            Console.WriteLine("ksndlksald: "+bsayi);
+
+
+
+            #region DateTime veri tipi
+
+            /*
+             
+             DateTime 
+             
+             */
+
+            Console.WriteLine(DateTime.Now);
+
+
+            DateTime dogumTarihi = new DateTime(1990,5,3,13,45,36);
+
+            Console.WriteLine(dogumTarihi);
+
+
+            #endregion
+
+
+
+
+
+
+
+        }
+    }
+}
